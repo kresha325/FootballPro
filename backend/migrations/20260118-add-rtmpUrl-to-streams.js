5cdf73eed28167c370f224330665e6c9dbc6a6c1
KRESHNIK\Urim <kresha325@icloud.com>
2026-01-21 01:55:14 +0100

    fix(migration): make add-rtmpUrl-to-streams migration idempotent

--- backend/migrations/20260118-add-rtmpUrl-to-streams.js
+++ backend/migrations/20260118-add-rtmpUrl-to-streams.js
@@ -2,12 +2,17 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('Streams', 'rtmpUrl', {
-      type: Sequelize.STRING,
-      allowNull: true,
-      defaultValue: null,
-      comment: 'RTMP ingest URL for YouTube/Twitch/other',
-    });
+    const table = await queryInterface.describeTable('Streams');
+    if (!table.rtmpUrl) {
+      await queryInterface.addColumn('Streams', 'rtmpUrl', {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+        comment: 'RTMP ingest URL for YouTube/Twitch/other',
+      });
+    } else {
+      console.log('Kolona rtmpUrl ekziston, nuk u shtua përsëri.');
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
