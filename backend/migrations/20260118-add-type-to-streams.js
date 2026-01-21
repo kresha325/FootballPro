5e67ca64be9871123a1e8156089a2d53809ac223
KRESHNIK\Urim <kresha325@icloud.com>
2026-01-21 18:37:24 +0100

    Make streams type migration idempotent

--- backend/migrations/20260118-add-type-to-streams.js
+++ backend/migrations/20260118-add-type-to-streams.js
@@ -2,14 +2,20 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('Streams', 'type', {
-      type: Sequelize.STRING,
-      allowNull: true, // Mund të bëhet false nëse të gjitha stream-et duhet ta kenë këtë fushë
-      defaultValue: null
-    });
+    const table = await queryInterface.describeTable('Streams');
+    if (!table.type) {
+      await queryInterface.addColumn('Streams', 'type', {
+        type: Sequelize.STRING,
+        allowNull: true, // Mund të bëhet false nëse të gjitha stream-et duhet ta kenë këtë fushë
+        defaultValue: null
+      });
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('Streams', 'type');
+    const table = await queryInterface.describeTable('Streams');
+    if (table.type) {
+      await queryInterface.removeColumn('Streams', 'type');
+    }
   }
 };
