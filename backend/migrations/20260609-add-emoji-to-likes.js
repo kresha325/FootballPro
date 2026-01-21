3aef4ca239510794be88d50f19c050fca03ca86a
KRESHNIK\Urim <kresha325@icloud.com>
2026-01-21 18:42:44 +0100

    Make emoji likes migration idempotent

--- backend/migrations/20260609-add-emoji-to-likes.js
+++ backend/migrations/20260609-add-emoji-to-likes.js
@@ -2,14 +2,20 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn("Likes", "emoji", {
-      type: Sequelize.STRING,
-      allowNull: true,
-      defaultValue: null,
-    });
+    const table = await queryInterface.describeTable("Likes");
+    if (!table.emoji) {
+      await queryInterface.addColumn("Likes", "emoji", {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+      });
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn("Likes", "emoji");
+    const table = await queryInterface.describeTable("Likes");
+    if (table.emoji) {
+      await queryInterface.removeColumn("Likes", "emoji");
+    }
   },
 };
