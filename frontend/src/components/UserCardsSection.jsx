82fce677d9edb9ce31c124efbf0db2875448a374
KRESHNIK\Urim <kresha325@icloud.com>
2026-01-23 13:29:26 +0100

    Hide featured players scrollbar on mobile

--- frontend/src/components/UserCardsSection.jsx
+++ frontend/src/components/UserCardsSection.jsx
@@ -72,7 +72,7 @@ const UserCardsSection = () => {
 
   return (
     <div className="mb-8">
-      <div className="flex gap-4 overflow-x-auto pb-2 snap-x snap-mandatory flex-nowrap">
+      <div className="flex gap-4 overflow-x-auto pb-2 snap-x snap-mandatory flex-nowrap hide-scrollbar-mobile">
         {profiles.map(profile => (
           <div key={profile.id} className="bg-white dark:bg-gray-800 rounded-xl shadow-lg overflow-hidden relative flex flex-col min-w-[220px] snap-start">
             <div className="relative h-48 w-full overflow-hidden">
