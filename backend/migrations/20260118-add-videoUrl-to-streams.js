a8ff0d40e944ca4b768fc04a70722d12a00b2535
KRESHNIK\Urim <kresha325@icloud.com>
2026-03-04 20:04:49 +0100

    Make add-videoUrl-to-streams migration idempotent

--- backend/migrations/20260118-add-videoUrl-to-streams.js
+++ backend/migrations/20260118-add-videoUrl-to-streams.js
@@ -2,15 +2,21 @@
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
-    await queryInterface.addColumn('Streams', 'videoUrl', {
-      type: Sequelize.STRING,
-      allowNull: true,
-      defaultValue: null,
-      comment: 'URL e videos së regjistruar ose stream-it',
-    });
+    const table = await queryInterface.describeTable('Streams');
+    if (!table.videoUrl) {
+      await queryInterface.addColumn('Streams', 'videoUrl', {
+        type: Sequelize.STRING,
+        allowNull: true,
+        defaultValue: null,
+        comment: 'URL e videos së regjistruar ose stream-it',
+      });
+    }
   },
 
   down: async (queryInterface, Sequelize) => {
-    await queryInterface.removeColumn('Streams', 'videoUrl');
+    const table = await queryInterface.describeTable('Streams');
+    if (table.videoUrl) {
+      await queryInterface.removeColumn('Streams', 'videoUrl');
+    }
   }
 };
