cef0d1facb605c304b5f56915832ddebce161b97
KRESHNIK\Urim <kresha325@icloud.com>
2026-03-25 18:57:04 +0100

    Krijo migration për tabelën UserAchievements për gamification

---
+++ backend/migrations/20260325-create-user-achievements.js
@@ -0,0 +1,40 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('UserAchievements', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      achievementId: {
+        type: Sequelize.INTEGER,
+        allowNull: false
+      },
+      unlockedAt: {
+        type: Sequelize.DATE,
+        allowNull: true
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP')
+      }
+    });
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('UserAchievements');
+  }
+};
